0788098858a463e12528fa805a03023739dde8f4
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2022-09-03 10:37:17 +0530

    UTF-8 removed from defaultLocale in configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -33,7 +33,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN.UTF-8";
+  i18n.defaultLocale = "en_IN";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
