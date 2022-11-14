460c5987fb41c1b1bbf7c64cae5c7e39cb3515da
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2022-11-14 16:53:11 +0530

    nvim-dap related plugins installed BUT NOT CONFIGURED

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -85,22 +85,6 @@ return packer.startup(function(use)
       { 'rafamadriz/friendly-snippets' },
     }
   }
-  --[[ -- cmp plugins ]]
-  --[[ use("hrsh7th/nvim-cmp") -- The completion plugin ]]
-  --[[ use("hrsh7th/cmp-buffer") -- buffer completions ]]
-  --[[ use("hrsh7th/cmp-path") -- path completions ]]
-  --[[ use("hrsh7th/cmp-cmdline") -- cmdline completions ]]
-  --[[ use("saadparwaiz1/cmp_luasnip") -- snippet completions ]]
-  --[[ use("hrsh7th/cmp-nvim-lsp") ]]
-
-  --[[ -- snippets ]]
-  --[[ use("L3MON4D3/LuaSnip") --snippet engine ]]
-  --[[ use("rafamadriz/friendly-snippets") -- a bunch of snippets to use ]]
-
-  -- LSP
-  --[[ use("neovim/nvim-lspconfig") -- enable LSP ]]
-  --[[ use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for ]]
-  --[[ use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters ]]
 
   -- Telescope
   use("nvim-telescope/telescope.nvim")
@@ -125,6 +109,11 @@ return packer.startup(function(use)
   use("https://git.sr.ht/~whynothugo/lsp_lines.nvim")
   use 'Yazeed1s/oh-lucy.nvim'
   use({"rose-pine/neovim", as = "rose-pine"})
+
+
+  use 'mfussenegger/nvim-dap'
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use 'leoluz/nvim-dap-go'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
