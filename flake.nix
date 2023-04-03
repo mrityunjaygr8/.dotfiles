14f8e061183cd775ec896ec32f2c2a4c6ab4bf44
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-04-03 14:48:08 +0530

    lanzaboote code back in for kharkanas

--- flake.nix
+++ flake.nix
@@ -43,22 +43,22 @@
 
           modules = [
             ./system/kharkanas-configuration.nix
-            # lanzaboote.nixosModules.lanzaboote
-            # ({config, pkgs, lib, ...}: {
-            #   boot.bootspec.enable = true;
-            #
-            #   environment.systemPackages = [
-            #     # For debugging and troubleshooting Secure Boot.
-            #     pkgs.sbctl
-            #   ];
-            #
-            #   boot.loader.systemd-boot.enable = lib.mkForce false;
-            #
-            #   boot.lanzaboote = {
-            #       enable = true;
-            #       pkiBundle = "/etc/secureboot";
-            #     };
-            # })
+            lanzaboote.nixosModules.lanzaboote
+            ({ config, pkgs, lib, ... }: {
+              boot.bootspec.enable = true;
+
+              environment.systemPackages = [
+                # For debugging and troubleshooting Secure Boot.
+                pkgs.sbctl
+              ];
+
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/etc/secureboot";
+              };
+            })
           ];
 
         };
