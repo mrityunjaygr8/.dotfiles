01ae13bf572f12d0de83d18679dabe7725475d5b
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-06-21 12:42:45 +0530

    added fish script to set WAYLAND_DISPLAY env
    this fixes the issue in helix of the correct clipboard provider not being set

--- users/mgr8/home.nix
+++ users/mgr8/home.nix
@@ -466,6 +466,30 @@ in
 
         direnv hook fish | source
         set -g direnv_fish_mode disable_arrow
+        # The WAYLAND_DISPLAY env is not being set in terminals other than GNOME CONSOLE.
+        # This was creating a problem when using helix, as it as not using wayland specific 
+        # clipboard provider due to this.
+        # This function is a workaround for setting the WAYLAND_DISPLAY env
+        if set -q "WAYLAND_DISPLAY"
+            exit
+        end
+
+        if set -q "XDG_SESSION_TYPE"
+            echo "XDG_SESSION_TYPE is set"
+            set SESSION_TYPE "$XDG_SESSION_TYPE"      
+
+            if test "$SESSION_TYPE" = "wayland"
+                echo "Setting WAYLAND_DISPLAY to 'wayland-0'"
+                set -Ux WAYLAND_DISPLAY "wayland-0"
+                exit
+            else
+                echo "SESSION_TYPE is not 'wayland', exiting"
+                exit
+            end
+        else
+            echo "XDG_SESSION_TYPE is unset, exiting"
+            exit
+        end
       '';
       shellAliases = {
         rm = "rm -i";
