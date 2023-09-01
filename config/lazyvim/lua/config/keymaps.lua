bb85764c4a9873c74893d5e11167c40c801fc36e
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-09-01 20:57:31 +0530

    new lazyvim keymaps added

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,40 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- Move to window using the <alt> hjkl keys
+map("n", "<A-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<A-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<A-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<A-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+-- Move Lines
+map("n", "<A-J>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-K>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-J>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-K>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-J>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-K>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- save file
+map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save file" })
+map("n", "<leader>C", "<cmd>q<cr><esc>", { desc = "Close buffer" })
+
+
+-- windows
+map("n","<leader>Ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n","<leader>Wd", "<C-W>c", { desc = "Delete window", remap = true })
+map("n","<leader>W-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n","<leader>W|", "<C-W>v", { desc = "Split window right", remap = true })
