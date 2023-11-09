b914cce598bddf59913f6df0c83542e467c4e37a
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2023-11-09 13:16:19 +0530

    old nvim renamed

---
+++ config/nvim_old/lua/user/colorscheme.lua
@@ -0,0 +1,14 @@
+--[[ local status_ok, rose_pine = pcall(require, "rose-pine") ]]
+--[[ if not status_ok then ]]
+--[[   return ]]
+--[[ end ]]
+--[[ rose_pine.setup({ ]]
+--[[   disable_italics = true ]]
+--[[ }) ]]
+--[[]]
+--[[ -- vim.g.catppuccin_flavour = "mocha" ]]
+--[[ -- catpuccin.setup() ]]
+--[[ -- vim.cmd([[colorscheme catppuccin]) ]]
+--[[ vim.cmd([[colorscheme rose-pine]) ]]
+vim.opt.background = "dark"
+vim.cmd("colorscheme github_dark")
