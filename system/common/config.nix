4444e3984e6566478657a64420d6a894a668b9ef
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-03-25 21:48:37 +0530

    some packages removed from kharkanas config

--- system/common/config.nix
+++ system/common/config.nix
@@ -76,9 +76,6 @@
     extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     initialPassword = "password";
     uid = 1000;
-    packages = with pkgs; [
-      firefox
-    ];
   };
   programs.zsh.enable = true;
   users.extraUsers.mgr8 = {
@@ -88,7 +85,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    gcc
+    # gcc
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     bind
