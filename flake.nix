1d1d216cf827da7b4ecd92c8f2992643a9baeb52
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-03-25 21:47:11 +0530

    blender removed from kharkanas config

--- flake.nix
+++ flake.nix
@@ -61,7 +61,6 @@
               environment.systemPackages = [
                 # For debugging and troubleshooting Secure Boot.
                 pkgs.sbctl
-                pkgs.blender
               ];
 
               boot.loader.systemd-boot.enable = lib.mkForce false;
