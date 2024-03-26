be23c6dee6975cad66ee0d5a32bcf83a9bc690c4
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-03-26 17:57:28 +0530

    firefox added back as system package

--- system/common/config.nix
+++ system/common/config.nix
@@ -88,6 +88,7 @@
     # gcc
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    firefox
     # bind
     # htop
     # zsh
